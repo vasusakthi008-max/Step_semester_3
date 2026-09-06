f83a6e498bb05b3d8f7644a6c63f26952ef05198
vasusakthi008-max <vasusakthi008@gmail.com>
2026-09-06 11:13:40 +0530

    Updated session_1
    This class provides three methods to check if a given string is a palindrome: iteratively, recursively, and using array reversal.

---
+++ class_problems/PalindromeChecker.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    static boolean isPalindromeIterative(String text) {
+        int i = 0, j = text.length() - 1;
+
+        while (i < j) {
+            if (text.charAt(i) != text.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static boolean isPalindromeRecursive(String text) {
+        if (text.length() <= 1)
+            return true;
+
+        if (text.charAt(0) != text.charAt(text.length() - 1))
+            return false;
+
+        return isPalindromeRecursive(text.substring(1, text.length() - 1));
+    }
+
+    static boolean isPalindromeArrayReversal(String text) {
+        char[] arr = text.toCharArray();
+
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        return text.equals(new String(arr));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        System.out.println("Iterative: " +
+                (isPalindromeIterative(text) ? "Palindrome" : "Not Palindrome"));
+
+        System.out.println("Recursive: " +
+                (isPalindromeRecursive(text) ? "Palindrome" : "Not Palindrome"));
+
+        System.out.println("Array Reversal: " +
+                (isPalindromeArrayReversal(text) ? "Palindrome" : "Not Palindrome"));
+    }
+}
