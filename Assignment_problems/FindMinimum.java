c5af970b02c023f992142fbbfee6f82f57964386
vasusakthi008-max <vasusakthi0082gmail.com>
2026-09-06 11:49:39 +0530

    updated session_3

---
+++ Assignment_problems/FindMinimum.java
@@ -0,0 +1,39 @@
+package Assignment_problems;
+
+import java.util.*;
+
+public class FindMinimum {
+
+    static int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Minimum element: " + findMin(nums));
+    }
+}
