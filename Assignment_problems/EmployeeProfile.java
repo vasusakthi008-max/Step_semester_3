3bcf0373aa9b9fda039b401880db611bf75e56f8
vasusakthi008-max <vasusakthi0082gmail.com>
2026-09-18 23:54:24 +0530

    updated session_5

--- Assignment_problems/EmployeeProfile.java
+++ Assignment_problems/EmployeeProfile.java
@@ -6,7 +6,6 @@ class Employee {
     double salary;
     boolean isIntern;
 
-    // Constructor for permanent employee
     public Employee(String empId, String empName, double salary) {
         this.empId = empId;
         this.empName = empName;
@@ -14,13 +13,11 @@ class Employee {
         this.isIntern = false;
     }
 
-    // Constructor for intern
     public Employee(String empId, String empName) {
         this(empId, empName, 0);
         this.isIntern = true;
     }
 
-    // Print employee profile
     public void printProfile() {
         System.out.println(empId + " | " + empName + " | Rs "
                 + salary + " | Intern: " + isIntern);
@@ -30,11 +27,9 @@ class Employee {
 public class EmployeeProfile {
     public static void main(String[] args) {
 
-        // Permanent employee
         Employee employee1 =
                 new Employee("E-101", "Divya", 65000);
 
-        // Intern
         Employee employee2 =
                 new Employee("E-102", "Arjun");
 
